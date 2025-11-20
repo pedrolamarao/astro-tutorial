c7701178a786a7024cd06b73b6fea0262fbea43b
Pedro Lamarão <pedro.lamarao@gmail.com>
2025-11-20 18:28:59 -0300

    unit 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://cute-mermaid-7a26f0.netlify.app/"
+});
