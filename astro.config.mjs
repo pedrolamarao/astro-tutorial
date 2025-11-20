a6027350d4c833c138c0abd7f74fb61acc24a9d7
Pedro Lamarão <pedro.lamarao@gmail.com>
2025-11-20 18:52:25 -0300

    unit 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cute-mermaid-7a26f0.netlify.app/"
+  site: "https://cute-mermaid-7a26f0.netlify.app/",
+  integrations: [preact()]
 });
--- src/blog/post-4.md
+++ src/blog/post-4.md
@@ -1,5 +1,4 @@
 ---
-layout: ../../layouts/MarkdownPostLayout.astro
 title: My Fourth Blog Post
 author: Pedro Lamarão
 description: "This post will show up on its own!"
